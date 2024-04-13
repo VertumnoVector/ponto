9c3854506d609c031a12d8325f51d01332e10f93
VertumnoVector <mottta.h@gmail.com>
2024-04-13 18:07:55 -0300

    UPDATE na rota de /logout (redirect para login)

--- api/logout.js
+++ api/logout.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     req.logOut(function (err) {
         if (err) { return next(err); }
-        req.flash("success_msg", "You have successfully logged out.");
+        req.flash("success_msg", "Saiu do sistema com sucesso.");
         res.redirect('/login');
     });
 });
